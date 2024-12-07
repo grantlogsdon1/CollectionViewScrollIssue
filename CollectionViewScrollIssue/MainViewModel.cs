8940ba901d5d13da436da1f558b5949d0289429e
Grant Logsdon <X2GLOGSD@southernco.com>
2024-12-06 19:07:33 -0500

    Renamed inner cv

--- CollectionViewScrollIssue/MainViewModel.cs
+++ CollectionViewScrollIssue/MainViewModel.cs
@@ -12,47 +12,47 @@ namespace CollectionViewScrollIssue
             {
                 new ParentItem
                 {
-                    Title = "Parent Item 1",
+                    Title = "Inner CollectionView 1",
                     ChildItems = new ObservableCollection<string> { "Child 1", "Child 2", "Child 3", "Child 4", "Child 5", "Child 6", "Child 7", "Child 8", "Child 9", "Child 10", }
                 },
                 new ParentItem
                 {
-                    Title = "Parent Item 2",
+                    Title = "Inner CollectionView 2",
                     ChildItems = new ObservableCollection<string> { "Child A", "Child B", "Child C" }
                 },
                 new ParentItem
                 {
-                    Title = "Parent Item 3",
+                    Title = "Inner CollectionView 3",
                     ChildItems = new ObservableCollection<string> { "Child X", "Child Y", "Child Z" }
                 },
                 new ParentItem
                 {
-                    Title = "Parent Item 4",
+                    Title = "Inner CollectionView 4",
                     ChildItems = new ObservableCollection<string> { "Child 1", "Child 2", "Child 3", "Child 4", "Child 5", "Child 6", "Child 7", "Child 8", "Child 9", "Child 10", }
                 },
                 new ParentItem
                 {
-                    Title = "Parent Item 5",
+                    Title = "Inner CollectionView 5",
                     ChildItems = new ObservableCollection<string> { "Child 1", "Child 2", "Child 3", "Child 4", "Child 5", "Child 6", "Child 7", "Child 8", "Child 9", "Child 10", }
                 },
                 new ParentItem
                 {
-                    Title = "Parent Item 6",
+                    Title = "Inner CollectionView 6",
                     ChildItems = new ObservableCollection<string> { "Child 1", "Child 2", "Child 3", "Child 4", "Child 5", "Child 6", "Child 7", "Child 8", "Child 9", "Child 10", }
                 },
                 new ParentItem
                 {
-                    Title = "Parent Item 7",
+                    Title = "Inner CollectionView 7",
                     ChildItems = new ObservableCollection<string> { "Child 1", "Child 2", "Child 3", "Child 4", "Child 5", "Child 6", "Child 7", "Child 8", "Child 9", "Child 10", }
                 },
                 new ParentItem
                 {
-                    Title = "Parent Item 8",
+                    Title = "Inner CollectionView 8",
                     ChildItems = new ObservableCollection<string> { "Child 1", "Child 2", "Child 3", "Child 4", "Child 5", "Child 6", "Child 7", "Child 8", "Child 9", "Child 10", }
                 },
                 new ParentItem
                 {
-                    Title = "Parent Item 9",
+                    Title = "Inner CollectionView 9",
                     ChildItems = new ObservableCollection<string> { "Child 1", "Child 2", "Child 3", "Child 4", "Child 5", "Child 6", "Child 7", "Child 8", "Child 9", "Child 10", }
                 },
             };
